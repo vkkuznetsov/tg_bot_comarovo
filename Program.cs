705584c8abb9435472b3db5f60aed88533c255c3
Viktor K <vita.201581@yandex.ru>
2023-04-01 13:13:24 +0500

    tries menu

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -13,44 +18,127 @@ class Program
 {
     private static void Main(string[] args)
     {
+        
+        
         var tokenBot = "6261177727:AAF8js_sluqbRtMCYLv4DFLQBknS00PsokE";
         var botClient = new TelegramBotClient(tokenBot);
 
         botClient.StartReceiving(Update, Error);
 
-        Console.ReadLine();
+       
 
+        Console.ReadLine();
         
 
+
     }
-    static ReplyKeyboardMarkup replyKeyboardMarkup = new(new[] { new KeyboardButton("Создать Мероприятие"), new KeyboardButton("Инфо") }) { ResizeKeyboard = true };
+    static ReplyKeyboardMarkup replyKeyboardMarkupNewEvent = new(new[] { new KeyboardButton("Отослать всем"), new KeyboardButton("Отмена"),
+        new KeyboardButton("Отослать Некоторым(?)") }) { ResizeKeyboard = true };
+
+    static ReplyKeyboardMarkup replyKeyboardMarkup = new(new[] { new KeyboardButton("Создать Мероприятие"), 
+        new KeyboardButton("Инфо") }) { ResizeKeyboard = true };
+
+    static ReplyKeyboardMarkup replyKeyboardMarkupComeBack = new(new[] { new KeyboardButton("Вернуться"),
+        new KeyboardButton("Инфо") })
+    { ResizeKeyboard = true };
+
+    static List<BotCommand> commands = new List<BotCommand>();
+    
 
     private static Task Error(ITelegramBotClient botClient, Exception ex, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+
+
     private static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
     {
         var message = update.Message;
-        // логи сообщений боту
-        await Console.Out.WriteLineAsync($"Получено сообщение от | {DefineWhatTheName(message.Chat.Id)} | текст: \"{message.Text}\" " +
-            $"| время: {message.Date} |{message.Chat.Username}" ); 
 
-        // ответ на слово привет
-        if (message.Text == "123")
-        {
-            await botClient.SendTextMessageAsync(message.Chat.Id,"privet",replyMarkup: replyKeyboardMarkup);
 
-            return;
-        }
-        else if (message.Text.ToLower() == "создать мероприятие")
+        commands.Add(new BotCommand { Command = @"start", Description = @"Стартуем комаровцы" });
+        commands.Add(new BotCommand { Command = @"help", Description = @"Макака не знает функций" });
+        commands.Add(new BotCommand { Command = @"create", Description = @"Создаем тусу джусу" });
+        commands.Add(new BotCommand { Command = @"thanks", Description = @"Сказать спасибо разработчикам" });
+        await botClient.SetMyCommandsAsync(commands);
+        
+        if(message.Type == MessageType.Text)
         {
-            await botClient.SendTextMessageAsync(message.Chat.Id, "Создем меропиятие", replyMarkup: replyKeyboardMarkup);
+            // логи сообщений боту
+            string logLine = $"Получено сообщение от |\t {DefineWhatTheName(message.Chat.Id)} |\t текст: \"{message.Text}\" " +
+            $"|\t время: {message.Date} |{message.Chat.Username}";
+
+            // вывод на консоль
+            await Console.Out.WriteLineAsync(logLine);
+
+            // вывод в файл
+            string fileName = @"C:\Users\vita2\source\repos\botTG\botTG\logs.txt";
+            
+
+
+
+            using (StreamWriter writer = new StreamWriter(fileName, true)) // the second parameter indicates we want to append to the file
+            {
+                writer.WriteLine(logLine);
+            }
+
+
+            // ответ на слово привет
+            if (message.Text == @"/start")
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Приветсвтую комаровец! \n Рад что вижу тебя здесь! " +
+                    "\n Открой меню и выбери то что хочешь сделать", replyMarkup: replyKeyboardMarkup);
+
+                return;
+            }
+            if (message.Text == @"/help")
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                await botClient.SendTextMessageAsync(message.Chat.Id, "я если че тоже не знаю функций", replyMarkup: replyKeyboardMarkup);
+
+                return;
+            }
+            if (message.Text == @"/create")
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Лееее мужик че тусу хочешь сделать?", replyMarkup: replyKeyboardMarkup);
+
+                return;
+            }
+            if (message.Text == @"/thanks")
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                await botClient.SendTextMessageAsync(message.Chat.Id, "угу", replyMarkup: replyKeyboardMarkup);
+
+                return;
+            }
+
+            else if (message.Text.ToLower() == "создать мероприятие")
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Создем меропиятие\n\n Кому будем отсылать рассылку? \n Выбери 1 из 3 вариантов",
+                    replyMarkup: replyKeyboardMarkupNewEvent);
+
+            }
+            if (message.Text.ToLower() == "отослать всем")
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Впиши сюда то что будешь рассылать и отправь");
+            }
+
+
         }
         
+        
+
+
+        
+
 
     }
+    
+
     
 
     public static string DefineWhatTheName(long chatId)
